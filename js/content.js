6d5cafe246984485bf80a08e6a4584452dac4edd
Jonathan Rogers <47063717+jd721rogers@users.noreply.github.com>
2019-08-26 12:38:17 -0500

    this is broke asf

--- js/content.js
+++ js/content.js
@@ -7,6 +7,7 @@ $( function checkForResults() {
         // Change headers
 
         $( "[data-property='courseTitle']" ).slice(0,1).text( "Basic Course Info" );  
+        $( "[data-property='attribute']" ).slice(0,1).text("Location");
 
         // Change each row
         //
@@ -17,29 +18,33 @@ $( function checkForResults() {
             // Getting data from data
             courseName = $( "[data-property='courseTitle']" ).slice(i,i+1).children("a").text();
             courseType = $( "[data-property='courseTitle']" ).slice(i,i+1).children("span").text();
-            attrLength = $( "[data-property='attribute']" ).slice(i,i+1).find("span").length;
-            switch(attrLength) {
-                case 0:
-                    break;
-                // case 1:
-                //     location = $( "[data-property='attribute']" ).slice(1,2).find("span").slice(0,1).text();
-                //     break;
-                // case 2:
-                //     location = $( "[data-property='attribute']" ).slice(1,2).find("span").slice(0,1).text();
-                //     courseAttr = $( "[data-property='attribute']" ).slice(1,2).find("span").slice(1,2).text();
-                //     break;
-            }
+            // attrLength = $( "[data-property='attribute']" ).slice(i,i+1).find("span").length;
+            // switch(attrLength) {
+            //     case 0:
+            //         break;
+            //     case 1:
+            //         location = $( "[data-property='attribute']" ).slice(i,i+1).find("span").text();
+            //         break;
+            //     case 2:
+            //         location = $( "[data-property='attribute']" ).slice(i,i+1).find("span").text();
+            //         courseAttr = $( "[data-property='attribute']" ).slice(i,i+1).find("span").text();
+            //         break;
+            // }
+            attributes = $( "[data-property='attribute']" ).slice(i-1,i).detach();
+            courseInfo = $( "[data-property='courseTitle']" ).slice(i-1,i);
+            attributes.insertAfter(courseInfo);
             instrMethod = $( "[data-property='instructionalMethod']" ).slice(i,i+1).text();
-            status = $( "[data-property='status']" ).slice(i,i+1).children("span").innerText;
+            status = $( "[data-property='status']" ).slice(i,i+1).find("span").text();
             remainingSpots = (status == "FULL") ? "0" : status;
-
+            instructorName = $( "[data-property='instructor']" ).slice(i,i+1).find("a").text();
+            instructorEmail = $( "[data-property='instructor']" ).slice(i,i+1).find("a").attr("href");
             // Reformatting table
-            // $( "[data-property='courseTitle']" ).slice(i,i+1).remove();
-            console.log("ran on row " + i.toString());
+            $( "[data-property='courseTitle']" ).slice(i,i+1).remove();
+            console.log("Row " + i.toString() + ": " + remainingSpots + " seats available with " + instructorName);
             //var newHTML = 
-
+            // newHTML.insertBefore(attributes);
         }
-        setInterval(checkForResults, 5000);
+        setInterval(checkForResults, 500);
     }
     // If there are no results, check again in half a second
     else {
